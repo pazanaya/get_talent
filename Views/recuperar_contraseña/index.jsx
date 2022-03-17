fc276c2233aed8f2c2a2b94acf35cc0d927b2d89
pazanaya <ada_hack329@hackademy.mx>
2022-03-17 11:18:43 -0600

    agregue imagen y recuperar contraseña

---
+++ Views/recuperar_contraseña/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+
+
+const Recuperarcontraseña = () => {
+  return (
+    <View>
+      <Text> Recupera tu contraseña </Text>
+    </View>
+  );
+};
+
+export default Recuperarcontraseña;
